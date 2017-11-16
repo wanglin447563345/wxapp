f7a57e5c0699f9083abc1a813d5dcba26245adf8
wl <447563345@qq.com>
2017-11-16 19:46:06 +0800

    电站分析页日期选择下拉

--- src/routes/PlantDetail/Analysis/components/Index.js
+++ src/routes/PlantDetail/Analysis/components/Index.js
@@ -3,14 +3,32 @@ import PropTypes from 'prop-types'
 import { Tabs, DatePicker, List } from 'antd-mobile'
 import enUs from 'antd-mobile/lib/date-picker/locale/en_US'
 import Header from '../../../../components/Header'
+import AnalysisDayChart from './subComponents/AnalysisDayChart'
+// 引入 ECharts 主模块
+import echarts from 'echarts/lib/echarts'
 const nowTimeStamp = Date.now()
-const date = new Date(nowTimeStamp)
-const month = new Date(nowTimeStamp)
-
+const now = new Date(nowTimeStamp)
+// 月份格式化
+function formatMonth (date) {
+  /* eslint no-confusing-arrow: 0 */
+  const pad = n => n < 10 ? `0${n}` : n;
+  const dateStr = `${date.getFullYear()}-${pad(date.getMonth() + 1)}`;
+  console.log(`${dateStr}`)
+  return `${dateStr}`;
+}
+// 年格式化
+function formatYear (date) {
+  /* eslint no-confusing-arrow: 0 */
+  const pad = n => n < 10 ? `0${n}` : n;
+  const dateStr = `${date.getFullYear()}`;
+  console.log(`${dateStr}`)
+  return `${dateStr}`;
+}
 class Analysis extends React.Component {
   state = {
-    date: date,
-    month: month
+    date: now,
+    month: now,
+    year:now
   }
   render () {
     const { query } = this.props.location
@@ -21,6 +39,52 @@ class Analysis extends React.Component {
       { title: '年', sub: '3' },
       { title: '累计', sub: '4' }
     ]
+    const options=[{
+      name: '南宁-曼芭',
+      type: 'line',
+      smooth: true,
+      lineStyle: {
+        normal: {
+          width: 1
+        }
+      },
+      itemStyle: {
+        normal: {
+          color: 'rgb(137,189,27)'
+        }
+      },
+      data: [96.3,96.4,97.5,95.6,98.1,94.8,89.6,94.1,80.1,52.4,75.8,94.7]
+    }, {
+      name: '桂林-曼芭',
+      type: 'line',
+      smooth: true,
+      lineStyle: {
+        normal: {
+          width: 1
+        }
+      },
+      itemStyle: {
+        normal: {
+          color: 'rgb(0,136,212)'
+        }
+      },
+      data: [97.3,99.2,99.3,100.0,99.6,90.6,80.0,91.5,69.8,67.5,90.4,84.9]
+    }, {
+      name: '南宁-甲米',
+      type: 'line',
+      smooth: true,
+      lineStyle: {
+        normal: {
+          width: 1
+        }
+      },
+      itemStyle: {
+        normal: {
+          color: 'rgb(219,50,51)'
+        }
+      },
+      data: [84.2,81.0,67.5,72.1,43.7,88.5,91.9,101.8,79.7,87.6,92.9,0]
+    }]
     return (
       <div className='analysis'>
         <div className='detail_header'>
@@ -41,20 +105,21 @@ class Analysis extends React.Component {
                   value={this.state.date}
                   onChange={date => this.setState({ date })}
                 >
-                  <List.Item arrow='horizontal' />
+                  <List.Item arrow='horizontal' >日期</List.Item>
                 </DatePicker>
               </List>
+              <AnalysisDayChart options={options} />
             </div>
             <div className='tab_content'>
               <List className='date-picker-list' style={{ backgroundColor: 'white' }}>
                 <DatePicker
                   mode='month'
                   title='选择月份'
-                  format='YY-MM'
+                  format={(val) => `${formatMonth(val)}`}
                   value={this.state.month}
                   onChange={month => this.setState({ month })}
                 >
-                  <List.Item arrow='horizontal' />
+                  <List.Item arrow='horizontal'>月份</List.Item>
                 </DatePicker>
               </List>
             </div>
@@ -63,10 +128,11 @@ class Analysis extends React.Component {
                 <DatePicker
                   mode='year'
                   title='选择年份'
+                  format={(val) => `${formatYear(val)}`}
                   value={this.state.year}
                   onChange={year => this.setState({ year })}
                 >
-                  <List.Item arrow='horizontal' />
+                  <List.Item arrow='horizontal'>年份</List.Item>
                 </DatePicker>
               </List>
             </div>
---
+++ src/routes/PlantDetail/Analysis/components/subComponents/AnalysisDayChart.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+// 引入 ECharts 主模块
+import echarts from 'echarts/lib/echarts'
+// 引入柱状图
+import 'echarts/lib/chart/line'
+// 引入提示框和标题组件
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/title'
+
+class AnalysisDayChart extends Component {
+  componentDidMount () {
+    // 基于准备好的dom，初始化echarts实例
+    const chartId = document.getElementById('analysis_day_chart')
+    let myChart = echarts.init(chartId)
+
+    const analysisDayOption = {
+      backgroundColor: '#FFFFFF',
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          lineStyle: {
+            color: '#57617B'
+          }
+        }
+      },
+      legend: {
+        show:true,
+        icon: 'rect',
+        itemWidth: 14,
+        itemHeight: 5,
+        itemGap: 13,
+        data: ['南宁-曼芭', '桂林-曼芭', '南宁-甲米'],
+        textStyle: {
+          fontSize: 12,
+          color: '#F1F1F3'
+        }
+      },
+      grid: {
+        left: '12%',
+        right: '3%',
+        bottom: '15%',
+        top: '12%'
+      },
+      xAxis: [{
+        type: 'category',
+        boundaryGap: false,
+        axisLine: {
+          lineStyle: {
+            color: '#57617B'
+          }
+        },
+        data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+      }],
+      yAxis: [{
+        type: 'value',
+        axisTick: {
+          show: false
+        },
+        axisLine: {
+          lineStyle: {
+            color: '#57617B'
+          }
+        },
+        axisLabel: {
+          margin: 10,
+          textStyle: {
+            fontSize: 14
+          }
+        },
+        splitLine: {
+          lineStyle: {
+            color: '#dbdbdb'
+          }
+        }
+      }],
+      series: this.props.options
+    };
+    // 绘制图表
+    myChart.setOption(analysisDayOption)
+  }
+  render () {
+    return (
+      <div id='analysis_day_chart' style={{ width: '98%', height: 200 }} />
+    )
+  }
+}
+AnalysisDayChart.propTypes = {
+  options: PropTypes.array.isRequired
+}
+
+export default AnalysisDayChart
